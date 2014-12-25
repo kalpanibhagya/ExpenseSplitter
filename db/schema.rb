2137fb8aba87156becad71a6a4951d12f989b8b2
sandarumk <sandarumk@gmail.com>
2014-12-25 23:10:14 +0530

    Added bootstrap and events

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141218145759) do
+ActiveRecord::Schema.define(version: 20141225145946) do
+
+  create_table "events", force: true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.datetime "date"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "user_id"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
